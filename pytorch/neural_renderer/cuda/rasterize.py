fixed a bug in the texture gradient

pytorch/neural_renderer/cuda/rasterize.py
@@ -99,7 +99,7 @@ class RasterizeFunction(Function):
         # initialize output buffers
         grad_faces = torch.zeros_like(faces, dtype=torch.float32).to(ctx.device).contiguous()
         if ctx.return_rgb:
-            grad_textures = torch.zeros_like(faces, dtype=torch.float32).to(ctx.device).contiguous()
+            grad_textures = torch.zeros_like(textures, dtype=torch.float32).to(ctx.device).contiguous()
         else:
             grad_textures = torch.zeros(1, dtype=torch.float32).to(ctx.device)
         

pytorch/tests/test_load_obj.py
@@ -8,33 +8,33 @@ import neural_renderer
 
 
 class TestCore(unittest.TestCase):
-    # def test_tetrahedron(self):
-    #     vertices_ref = np.array(
-    #         [
-    #             [1., 0., 0.],
-    #             [0., 1., 0.],
-    #             [0., 0., 1.],
-    #             [0., 0., 0.]],
-    #         'float32')
-    #     faces_ref = np.array(
-    #         [
-    #             [1, 3, 2],
-    #             [3, 1, 0],
-    #             [2, 0, 1],
-    #             [0, 2, 3]],
-    #         'int32')
+    def test_tetrahedron(self):
+        vertices_ref = np.array(
+            [
+                [1., 0., 0.],
+                [0., 1., 0.],
+                [0., 0., 1.],
+                [0., 0., 0.]],
+            'float32')
+        faces_ref = np.array(
+            [
+                [1, 3, 2],
+                [3, 1, 0],
+                [2, 0, 1],
+                [0, 2, 3]],
+            'int32')
 
-    #     vertices, faces = neural_renderer.load_obj('../../chainer/tests/data/tetrahedron.obj', False)
-    #     assert (np.allclose(vertices_ref, vertices))
-    #     assert (np.allclose(faces_ref, faces))
-    #     vertices, faces = neural_renderer.load_obj('../../chainer/tests/data/tetrahedron.obj', True)
-    #     assert (np.allclose(vertices_ref * 2 - 1.0, vertices))
-    #     assert (np.allclose(faces_ref, faces))
+        vertices, faces = neural_renderer.load_obj('../../chainer/tests/data/tetrahedron.obj', False)
+        assert (np.allclose(vertices_ref, vertices))
+        assert (np.allclose(faces_ref, faces))
+        vertices, faces = neural_renderer.load_obj('../../chainer/tests/data/tetrahedron.obj', True)
+        assert (np.allclose(vertices_ref * 2 - 1.0, vertices))
+        assert (np.allclose(faces_ref, faces))
 
-    # def test_teapot(self):
-    #     vertices, faces = neural_renderer.load_obj('../../chainer/tests/data/teapot.obj')
-    #     assert (faces.shape[0] == 2464)
-    #     assert (vertices.shape[0] == 1292)
+    def test_teapot(self):
+        vertices, faces = neural_renderer.load_obj('../../chainer/tests/data/teapot.obj')
+        assert (faces.shape[0] == 2464)
+        assert (vertices.shape[0] == 1292)
 
     def test_texture(self):
         renderer = neural_renderer.Renderer()