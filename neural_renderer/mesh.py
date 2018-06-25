204799abc14f74d47308d41e050010bdf1f9fe41
Nikos Kolotouros <nkolot@seas.upenn.edu>
2018-06-25 10:19:35 -0400

    fixed a bug in the parameter registration

--- neural_renderer/mesh.py
+++ neural_renderer/mesh.py
@@ -7,15 +7,15 @@ class Mesh(nn.Module):
 
     def __init__(self, vertices, faces, textures, texture_size=4):
         super(Mesh, self).__init__()
-        self.register_parameter('vertices', vertices)
-        self.register_buffer('faces', faces)
+        self.vertices = nn.Parameter(vertices)
+        self.faces = nn.Parameter(faces)
         self.num_vertices = self.vertices.shape[0]
         self.num_faces = self.faces.shape[0]
 
         # create textures
         if textures is None:
             shape = (self.num_faces, texture_size, texture_size, texture_size, 3)
-            self.register_parameter('textures', 0.05*torch.randn(*shape))
+            self.textures = nn.Parameter(0.05*torch.randn(*shape))
             self.texture_size = texture_size
         else:
             self.texture_size = textures.shape[0]
@@ -26,12 +26,12 @@ class Mesh(nn.Module):
             vertices, faces, textures = nr.load_obj(filename_obj,
                                                     normalization=normalization,
                                                     texture_size=texture_size,
-                                                    load_textures=True)
+                                                    load_texture=True)
         else:
             vertices, faces = nr.load_obj(filename_obj,
                                           normalization=normalization,
                                           texture_size=texture_size,
-                                          load_textures=False)
+                                          load_texture=False)
             textures = None
         return cls(vertices, faces, textures, texture_size)
 
