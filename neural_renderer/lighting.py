6083cb8e7ea9b4ce8e444c97a378f56b5dfabbd2
Nikos Kolotouros <nkolot@seas.upenn.edu>
2018-07-10 12:28:59 -0400

    create tensors directly if not numpy array

--- neural_renderer/lighting.py
+++ neural_renderer/lighting.py
@@ -2,9 +2,8 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
-def lighting(
-        faces, textures, intensity_ambient=0.5, intensity_directional=0.5, color_ambient=(1, 1, 1),
-        color_directional=(1, 1, 1), direction=(0, 1, 0)):
+def lighting(faces, textures, intensity_ambient=0.5, intensity_directional=0.5,
+             color_ambient=(1, 1, 1), color_directional=(1, 1, 1), direction=(0, 1, 0)):
 
     bs, nf = faces.shape[:2]
     device = faces.device
@@ -12,15 +11,15 @@ def lighting(
     # arguments
     # make sure to convert all inputs to float tensors
     if isinstance(color_ambient, tuple) or isinstance(color_ambient, list):
-        color_ambient = torch.from_numpy(np.array(color_ambient, np.float32)).to(device)
+        color_ambient = torch.tensor(color_ambient, dtype=torch.float32, device=device)
     elif isinstance(color_ambient, np.ndarray):
         color_ambient = torch.from_numpy(color_ambient).float().to(device)
     if isinstance(color_directional, tuple) or isinstance(color_directional, list):
-        color_directional = torch.from_numpy(np.array(color_directional, np.float32)).to(device)
+        color_directional = torch.tensor(color_directional, dtype=torch.float32, device=device)
     elif isinstance(color_directional, np.ndarray):
         color_directional = torch.from_numpy(color_directional).float().to(device)
     if isinstance(direction, tuple) or isinstance(direction, list):
-        direction = torch.from_numpy(np.array(direction, np.float32)).to(device)
+        direction = torch.tensor(direction, dtype=torch.float32, device=device)
     elif isinstance(direction, np.ndarray):
         direction = torch.from_numpy(direction).float().to(device)
     if color_ambient.ndimension() == 1:
@@ -56,5 +55,3 @@ def lighting(
     light = light[:,:,None, None, None, :]
     textures *= light
     return textures
-
-
