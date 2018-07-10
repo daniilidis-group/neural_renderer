2a7c913bac871f39ede497b67fa27d131bc3e404
Nikos Kolotouros <nkolot@seas.upenn.edu>
2018-07-10 12:45:53 -0400

    add some type checks

--- neural_renderer/look_at.py
+++ neural_renderer/look_at.py
@@ -3,41 +3,39 @@ import torch
 import torch.nn.functional as F
 
 
-def look_at(vertices, eye, at=None, up=None):
+def look_at(vertices, eye, at=[0, 0, 0], up=[0, 1, 0]):
     """
     "Look at" transformation of vertices.
     """
-    assert (vertices.ndimension() == 3)
+    if (vertices.ndimension() != 3):
+        raise ValueError('vertices Tensor should have 3 dimensions')
+
     device = vertices.device
 
-    batch_size = vertices.shape[0]
-    if at is None:
-        at = torch.from_numpy(np.array([0, 0, 0], np.float32)).to(device)
-    elif isinstance(at, list) or isinstance(at, tuple):
-        at = torch.from_numpy(np.array(at, np.float32)).to(device)
+    # if list or tuple convert to numpy array
+    if isinstance(at, list) or isinstance(at, tuple):
+        at = torch.tensor(at, dtype=torch.float32, device=device)
+    # if numpy array convert to tensor
     elif isinstance(at, np.ndarray):
         at = torch.from_numpy(at).to(device)
-    else:
+    elif torch.is_tensor(at):
         at.to(device)
 
-    if up is None:
-        up = torch.from_numpy(np.array([0, 1, 0], np.float32)).to(device)
-    elif isinstance(up, list) or isinstance(up, tuple):
-        up = torch.from_numpy(np.array(up, np.float32)).to(device)
+    if isinstance(up, list) or isinstance(up, tuple):
+        up = torch.tensor(up, dtype=torch.float32, device=device)
     elif isinstance(up, np.ndarray):
         up = torch.from_numpy(up).to(device)
-    else:
+    elif torch.is_tensor(up):
         up.to(device)
 
-    # if list or tuple convert to numpy array
     if isinstance(eye, list) or isinstance(eye, tuple):
-        eye = torch.from_numpy(np.array(eye, np.float32)).to(device)
-    # if numpy array convert to tensor
+        eye = torch.tensor(eye, dtype=torch.float32, device=device)
     elif isinstance(eye, np.ndarray):
         eye = torch.from_numpy(eye).to(device)
     else:
         eye = eye.to(device)
 
+    batch_size = vertices.shape[0]
     if eye.ndimension() == 1:
         eye = eye[None, :].repeat(batch_size, 1)
     if at.ndimension() == 1:
