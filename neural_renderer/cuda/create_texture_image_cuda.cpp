378989dcdae659c7021efda91ea20bf99cedf102
wangyida <yidawang.cn@gmail.com>
2021-06-05 11:41:00 +0200

    modify for pytorch 1.6 1.7 and 1.8

--- neural_renderer/cuda/create_texture_image_cuda.cpp
+++ neural_renderer/cuda/create_texture_image_cuda.cpp
@@ -10,6 +10,10 @@ at::Tensor create_texture_image_cuda(
 
 // C++ interface
 
+#ifndef AT_CHECK
+#define AT_CHECK TORCH_CHECK
+#endif
+
 #define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
